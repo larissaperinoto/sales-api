95d24970f61a860af022c69a0369ddaf0591876c
Larissa Perinoto <perinotolarissa@gmail.com>
2024-03-05 11:48:05 -0300

    chore: remove logs

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -4,8 +4,10 @@ const StatusCode = require('../utils/statusCode');
 async function login(req, res) {
   try {
     const response = await loginService.login(req.body);
+
     res.status(StatusCode.OK).json(response);
   } catch(e) {
+    console.log(e)
     res.status(e.status).json({ message: e.message });
   }
 };
