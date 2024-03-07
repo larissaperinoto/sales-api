eea16f9836b43c70f9154502d7cfe8b4f30ff24e
Larissa Perinoto <perinotolarissa@gmail.com>
2024-03-07 14:07:47 -0300

    feat: insere configuração de cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const errorMiddleware = require('./middlewares/error.middleware');
 
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 
 const router = require('./routes');
 
